9e9c593eeb22fcd5f8987141a1516d647eb18830
ogranesson <shresthaanshu555@gmail.com>
2024-10-25 14:40:39 +0200

    Metronome changes
    Added sound, more sliders and text

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonRange, IonLabel } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonRange, IonLabel, IonItem, IonList, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -8,31 +8,33 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonRange, IonHeader, IonToolbar, IonTitle, IonRange, IonLabel, IonContent, ExploreContainerComponent, FormsModule],
+  imports: [IonButton, IonList, IonItem, IonRange, IonHeader, IonToolbar, IonTitle, IonRange, IonLabel, IonContent, ExploreContainerComponent, FormsModule],
 })
 export class Tab1Page {
-  rotationAngle: number = -45;
+  rotationAngle: number = 0;
   tempo: number = 60;
   tempoTime: number = 1;
   interval!: ReturnType<typeof setInterval>; // because in Node, setInterval returns a Timeout object, while in the browser it returns a number
 
+  volume: number = 50;
+  beats: number = 4;
+
+  isPlaying: boolean = false;
+
+  audio = new Audio('assets/beat.mp3');
+
   constructor() {}
 
   ngOnInit() {
-    this.interval = setInterval(() => {
-      if (this.rotationAngle === -45) {
-        this.rotationAngle = 45;
-      } else {
-        this.rotationAngle = -45;
-      }
-    }, this.tempoTime * 1000);
+    this.audio.load();
   }
-
-  
 
   tempoChange() {
     this.tempoTime = 60/this.tempo;
+    this.metronome();
+  }
 
+  metronome() {
     clearInterval(this.interval);
     this.interval = setInterval(() => {
       if (this.rotationAngle == -45) {
@@ -41,6 +43,29 @@ export class Tab1Page {
       else {
         this.rotationAngle = -45;
       }
+
+      setTimeout(() => {
+        if (this.rotationAngle === 45 || this.rotationAngle === -45) {
+          this.playSound();
+        }
+      }, (this.tempoTime * 1000) / 2);
     }, this.tempoTime * 1000);
+  }
+
+  playSound() {
+    this.audio.currentTime = 0;
+    this.audio.play();
+  }
+
+  pause() {
+    if (this.isPlaying) {
+      clearInterval(this.interval);
+      this.rotationAngle = 0;
+      this.isPlaying = false;
+    }
+    else {
+      this.metronome();
+      this.isPlaying = true;
+    }
   }
 }
